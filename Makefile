0318a034467a55f9243f7fa6541e9a5f378cac58
Brian Maher <maherb@brimworks.com>
2009-05-18 23:34:21 -0700

    Fixed the build so it works with make-common.

--- Makefile
+++ Makefile
@@ -1,1 +1,4 @@
-include ../Makefile
+SRC:=$(abspath $(dir $(lastword $(MAKEFILE_LIST))))
+include $(or $(shell make-common),$(error make-common is not on your PATH!))
+
+include $(SRC)/module.mk
---
+++ module.mk
@@ -0,0 +1,29 @@
+pwd := $(pwd)
+
+include $(make-common.dir)/tool/cc.mk
+include $(make-common.dir)/tool/lua.mk
+include $(make-common.dir)/layout.mk
+
+_lib  := $(lua.lib.dir)/zlib.so
+_objs := $(call cc.c.to.o,$(addprefix $(pwd)/, \
+    lua_zlib.c \
+))
+
+all: | $(_lib)
+$(_lib): cc.libs += lua z
+$(_lib): $(_objs)
+	$(cc.so.rule)
+
+# How to run lua_zlib tests:
+.PHONY: lua_zlib.test
+test: | lua_zlib.test
+
+lua ?= lua
+lua_zlib.test: | $(lua.lib.dir)/zlib.so
+lua_zlib.test: lua.path += $(pwd)
+lua_zlib.test: $(wildcard $(pwd)/test*)
+	@mkdir -p $(tmp.dir)
+	cd $(tmp.dir); for t in $<; do \
+		echo "TESTING: $$t"; \
+		env -i $(lua.run) $(lua) $$t; \
+	done
