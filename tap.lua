5f41fa4fd8f7f84f62c100f40eecea78bea3d3f4
Jakob Nixdorf <flocke@shadowice.org>
2015-04-01 14:25:16 +0200

    Make the tap module compatible with lua 5.2

--- tap.lua
+++ tap.lua
@@ -1,10 +1,11 @@
+local tap_module = {}
+
 local os = require("os")
-module(..., package.seeall)
 
 local counter = 1
 local failed  = false
 
-function ok(assert_true, desc)
+function tap_module.ok(assert_true, desc)
    local msg = ( assert_true and "ok " or "not ok " ) .. counter
    if ( not assert_true ) then
       failed = true
@@ -16,6 +17,8 @@ function ok(assert_true, desc)
    counter = counter + 1
 end
 
-function exit()
+function tap_module.exit()
    os.exit(failed and 1 or 0)
 end
+
+return tap_module
